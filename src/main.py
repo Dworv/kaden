6dd14bdc3693636bbfae55b80e29dc901532b963
Dworv YT <dwarvyt@gmail.com>
2022-08-27 12:21:53 -0400

    feat: add bot start

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-from naff import Activity, ActivityType, Client, Intents
+from naff import Activity, ActivityType, Client
+
+from utils import read_config
 
 bot = Client(
     activity=Activity(
@@ -6,5 +8,10 @@ bot = Client(
         ActivityType.STREAMING,
         "https://github.com/dworv",
     ),
-    debug_scope=
+    debug_scope=read_config("debug_scope")
 )
+
+with open("TOKEN") as f:
+    token = f.readlines()[0].strip()
+
+bot.start(token)
