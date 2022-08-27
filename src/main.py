70085d8adcf9e0024101d122605d1922bd07bd32
Dworv YT <dwarvyt@gmail.com>
2022-08-27 16:10:26 -0400

    feat: implement exts

--- src/main.py
+++ src/main.py
@@ -1,15 +1,28 @@
+#!/usr/local/bin/python3.10
+import os
+
 from naff import Activity, ActivityType, Client
 
 from utils import read_config
 
 bot = Client(
     activity=Activity(
-        "through submissions.",
-        ActivityType.STREAMING,
+        "your submissions.",
+        ActivityType.WATCHING,
         "https://github.com/dworv",
     ),
-    debug_scope=read_config("debug_scope")
+    debug_scope=read_config("debug_scope"),
 )
+
+for root, _, files in os.walk("exts"):
+    for file in files:
+        if file.endswith(".py") and not file.startswith("__init__"):
+            file = file.removesuffix(".py")
+            path = os.path.join(root, file)
+            python_import_path = path.replace("/", ".").replace("\\", ".")
+
+            # load the extension
+            bot.load_extension(python_import_path)
 
 with open("TOKEN") as f:
     token = f.readlines()[0].strip()
