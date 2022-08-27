aa1ee1329fc37a94c6016ca14f612672dbcb2bf4
Dworv YT <dwarvyt@gmail.com>
2022-08-27 12:21:23 -0400

    feat: add utils for json

---
+++ src/utils/config.py
@@ -0,0 +1,13 @@
+from json import load
+
+def read_config(*fields: list[str]) -> str | int | float | dict | list:
+    """
+    Reads a field from the config file.
+
+    Use a different arg for each nested field name.
+    """
+    with open("config.json") as f:
+        item: dict = load(f)
+        for field in fields:
+            item = item.get(field)
+        return item
