5f2e375f30e9c23b327be9991ae1d32b7b0581e5
Dworv YT <dwarvyt@gmail.com>
2022-08-27 20:46:21 -0400

    feat: add logging and delete old cmds

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 #!/usr/local/bin/python3.10
 import os
+import logging
 
 from naff import Activity, ActivityType, Client
 
 from utils import read_config
+
+logging.basicConfig(level=logging.DEBUG)
 
 bot = Client(
     activity=Activity(
@@ -11,7 +14,7 @@ bot = Client(
         ActivityType.WATCHING,
         "https://github.com/dworv",
     ),
-    debug_scope=read_config("debug_scope"),
+    delete_unused_application_cmds=True
 )
 
 for root, _, files in os.walk("exts"):
