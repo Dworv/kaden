d88853b01d41edbebb1dba44b825addc941f2723
Dworv YT <dwarvyt@gmail.com>
2022-08-29 08:24:25 -0400

    feat: /comp end /comp new

--- src/exts/manage.py
+++ src/exts/manage.py
@@ -9,6 +9,10 @@ from naff import (
     slash_option,
 )
 
+from utils import val_tournament_desc, val_tournament_name, add_hours, success_embed, submit_button, info_embed, choose_comp
+from db import database, CompState
+from utils.embeds import error_embed
+
 
 class Manage(Extension):
     @slash_command("comp", default_member_permissions=Permissions.ADMINISTRATOR)
@@ -57,10 +61,24 @@ class Manage(Extension):
         self, ctx: InteractionContext, name: str, description: str, length: int
     ):
         """Create a new competition."""
+        if not await val_tournament_name(ctx, name):
+            return
+        if not await val_tournament_desc(ctx, description):
+            return
+        end = add_hours(length)
+        comp_id = database.create_competition(ctx.guild_id, ctx.author.id, name, description, end)
+        await ctx.send(embeds=success_embed(f"Competition `{name}` created! Display a nice message with a submit button using </comp show:69>"), components=submit_button(comp_id))
 
     @comp.subcommand("end")
     async def comp_end(self, ctx: InteractionContext):
         """Provoke an early end to an existing competition."""
+        if not database.get_guild_comps(ctx.guild_id, CompState.SUBMIT):
+            await ctx.send(embed=error_embed("There are no active competitions in your server."))
+            return
+        if (button_ctx := await choose_comp(ctx, "end", CompState.SUBMIT)) is None:
+            return
+        database.set_comp_state(button_ctx.values[0], CompState.PENDING)
+        await button_ctx.edit_origin(embeds=success_embed("Competition successfully terminated. The submissions are awaiting review."), components=[])
 
     @comp.subcommand("delay")
     @slash_option(
@@ -92,6 +110,10 @@ class Manage(Extension):
     @comp.subcommand("cancel")
     async def comp_cancel(self, ctx: InteractionContext):
         """Cancel a competition. CANNOT BE UNDONE!"""
+    
+    @comp.subcommand("show")
+    async def comp_show(self, ctx: InteractionContext):
+        """Display a competition accomponied by a submit button."""
 
 def setup(bot: Client):
     Manage(bot)
