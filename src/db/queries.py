e2908e6ececa2fd84831eef6e4a769b9c112bab6
Dworv YT <dwarvyt@gmail.com>
2022-08-30 16:00:23 -0400

    feat: more queries

--- src/db/queries.py
+++ src/db/queries.py
@@ -1,5 +1,6 @@
 import pathlib
 import sqlite3
+import time
 
 from .tables import comps_table, guilds_table, submissions_table
 from .enums import CompState
@@ -22,28 +23,80 @@ class DBClient:
 
     def create_competition(self, guild_id, creator_id, name, description, end):
         res = self.curs["comps"].execute(
-            "INSERT INTO comps (guild_id, creator_id, state, name, description, end) values (?, ?, ?, ?, ?, ?) RETURNING comp_id",
-            (guild_id, creator_id, CompState.SUBMIT.value, name, description, end),
+            "INSERT INTO comps (guild_id, creator_id, state, name, description, end, start) values (?, ?, ?, ?, ?, ?, ?) RETURNING comp_id",
+            (
+                guild_id,
+                creator_id,
+                CompState.SUBMIT.value,
+                name,
+                description,
+                end,
+                time.time(),
+            ),
         )
         comp_id = next(res)
         self.conns["comps"].commit()
         return comp_id
-    
+
     def set_comp_state(self, comp_id: int, state: CompState):
-        print(type(comp_id), type(state))
-        self.curs["comps"].execute("UPDATE comps SET state = ? WHERE comp_id = ?", (state.value, comp_id))
+        self.curs["comps"].execute(
+            "UPDATE comps SET state = ? WHERE comp_id = ?", (state.value, comp_id)
+        )
         self.conns["comps"].commit()
 
     def get_comp(self, comp_id):
         self.curs["comps"].execute("SELECT * FROM comps WHERE comp_id = ?", (comp_id,))
-        res = self.curs["comps"].fetchone()
-        self.conns["comps"].commit()
-        return res
-    
+        return self.curs["comps"].fetchone()
+
     def get_guild_comps(self, guild_id, state: CompState = None):
-        self.curs["comps"].execute("SELECT * FROM comps WHERE guild_id = ?", (guild_id,))
-        res = self.curs["comps"].fetchall()
+        if state is not None:
+            self.curs["comps"].execute(
+                "SELECT * FROM comps WHERE guild_id = ? AND state = ?",
+                (guild_id, state.value),
+            )
+        else:
+            self.curs["comps"].execute(
+                "SELECT * FROM comps WHERE guild_id = ?", (guild_id,)
+            )
+        return self.curs["comps"].fetchall()
+
+    def extend_comp_end(self, comp_id, length):
+        self.curs["comps"].execute(
+            "UPDATE comps SET end = end + ? WHERE comp_id = ?", (length, comp_id)
+        )
         self.conns["comps"].commit()
-        return [comp for comp in res if comp[3] == state.value] if isinstance(state, CompState) else res
+
+    def create_submission(self, comp_id, user_id, url):
+        res = self.curs["submissions"].execute(
+            "INSERT INTO submissions (comp_id, user_id, url, creation_date) values (?, ?, ?, ?, ?) RETURNING submission_id",
+            (comp_id, user_id, url, time.time(), 0),
+        )
+        submission_id = next(res)
+        self.conns["submissions"].commit()
+        return submission_id
+
+    def get_submission(self, submission_id):
+        self.curs["submissions"].execute(
+            "SELECT * FROM submissions WHERE submission_id = ?", (submission_id,)
+        )
+        return self.curs["submissions"].fetchone()
+
+    def get_comp_submissions(self, comp_id, scored: bool = None):
+        # sourcery skip: simplify-boolean-comparison
+        if scored is True:
+            self.curs["submissions"].execute(
+                "SELECT * FROM comps WHERE guild_id = ? AND NOT score = ?", (comp_id, 0)
+            )
+        if scored is False:
+            self.curs["submissions"].execute(
+                "SELECT * FROM comps WHERE guild_id = ? AND score = ?", (comp_id, 0)
+            )
+        else:
+            self.curs["submissions"].execute(
+                "SELECT * FROM comps WHERE guild_id = ?", (comp_id,)
+            )
+
+        return self.curs["submissions"].fetchall()
+
 
 database = DBClient()
