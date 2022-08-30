ef9537c07f6af01da0c068340174f6ee23c18ae0
Dworv YT <dwarvyt@gmail.com>
2022-08-30 16:00:44 -0400

    feat: finish management commands

--- src/exts/manage.py
+++ src/exts/manage.py
@@ -1,3 +1,5 @@
+import time
+
 from naff import (
     Extension,
     Client,
@@ -9,7 +11,17 @@ from naff import (
     slash_option,
 )
 
-from utils import val_tournament_desc, val_tournament_name, add_hours, success_embed, submit_button, info_embed, choose_comp
+from utils import (
+    val_tournament_desc,
+    val_tournament_name,
+    add_hours,
+    success_embed,
+    submit_button,
+    info_embed,
+    choose_comp,
+    autocomplete_contest,
+    comp_embed,
+)
 from db import database, CompState
 from utils.embeds import error_embed
 
@@ -66,21 +78,48 @@ class Manage(Extension):
         if not await val_tournament_desc(ctx, description):
             return
         end = add_hours(length)
-        comp_id = database.create_competition(ctx.guild_id, ctx.author.id, name, description, end)
-        await ctx.send(embeds=success_embed(f"Competition `{name}` created! Display a nice message with a submit button using </comp show:69>"), components=submit_button(comp_id))
+        comp_id = database.create_competition(
+            ctx.guild_id, ctx.author.id, name, description, end
+        )
+        await ctx.send(
+            embeds=success_embed(
+                f"Competition `{name}` created! Display a nice message with a submit button using </comp show:69>"
+            ),
+            components=submit_button(comp_id),
+        )
 
     @comp.subcommand("end")
-    async def comp_end(self, ctx: InteractionContext):
-        """Provoke an early end to an existing competition."""
-        if not database.get_guild_comps(ctx.guild_id, CompState.SUBMIT):
-            await ctx.send(embed=error_embed("There are no active competitions in your server."))
+    @slash_option(
+        "contest",
+        "Choose a contest to end.",
+        OptionTypes.INTEGER,
+        required=True,
+        autocomplete=True,
+    )
+    async def comp_end(self, ctx: InteractionContext, contest: int):
+        """Provoke an early end to a competition review the submissions."""
+        if contest not in [
+            comp[0] for comp in database.get_guild_comps(ctx.guild_id, CompState.SUBMIT)
+        ]:
+            await ctx.send(embed=error_embed("That contest is not availible."))
             return
-        if (button_ctx := await choose_comp(ctx, "end", CompState.SUBMIT)) is None:
-            return
-        database.set_comp_state(button_ctx.values[0], CompState.PENDING)
-        await button_ctx.edit_origin(embeds=success_embed("Competition successfully terminated. The submissions are awaiting review."), components=[])
+        database.set_comp_state(contest, CompState.PENDING)
+        await ctx.send(
+            embeds=success_embed(
+                "Competition successfully ended early. The submissions are now awaiting review."
+            )
+        )
 
-    @comp.subcommand("delay")
+    comp_end.autocomplete("contest")(autocomplete_contest(CompState.SUBMIT))
+
+    @comp.subcommand("extend")
+    @slash_option(
+        "contest",
+        "Choose a contest to extend.",
+        OptionTypes.INTEGER,
+        required=True,
+        autocomplete=True,
+    )
     @slash_option(
         "duration",
         "How long the tournament should stay open.",
@@ -104,16 +143,68 @@ class Manage(Extension):
             SlashCommandChoice("4 Weeks", 4 * 7 * 24),
         ],
     )
-    async def comp_delay(self, ctx: InteractionContext, duration: int):
-        """Delay an existing competition."""
-    
+    async def comp_extend(self, ctx: InteractionContext, contest: int, duration: int):
+        """Delay an existing competition. Max contest length is 4 weeks."""
+        if contest not in [
+            comp[0] for comp in database.get_guild_comps(ctx.guild_id, CompState.SUBMIT)
+        ]:
+            await ctx.send(embed=error_embed("That contest is not availible."))
+            return
+        current_end: int = database.get_comp(contest)[6]
+        new_end = min(
+            current_end + duration * 60 * 60, round(time.time()) + 60 * 60 * 24 * 7 * 4
+        )  # cap the end time to 4 weeks in future
+        extension = new_end - current_end
+        database.extend_comp_end(contest, extension)
+        await ctx.send(
+            embeds=success_embed(
+                f"The competition has been extended until <t:{new_end}:F>"
+            )
+        )
+
+    comp_extend.autocomplete("contest")(autocomplete_contest(CompState.SUBMIT))
+
     @comp.subcommand("cancel")
-    async def comp_cancel(self, ctx: InteractionContext):
-        """Cancel a competition. CANNOT BE UNDONE!"""
-    
+    @slash_option(
+        "contest",
+        "Choose a contest to cancel.",
+        OptionTypes.INTEGER,
+        required=True,
+        autocomplete=True,
+    )
+    async def comp_cancel(self, ctx: InteractionContext, contest: int):
+        """Cancel a competition. FORGETS ALL SUBMISSIONS CANNOT BE UNDONE!"""
+        if contest not in [
+            comp[0] for comp in database.get_guild_comps(ctx.guild_id, CompState.SUBMIT)
+        ]:
+            await ctx.send(embed=error_embed("That contest is not availible."))
+            return
+        database.set_comp_state(contest, CompState.OVER)
+        await ctx.send(
+            embeds=success_embed("Competition successfully removed."), components=[]
+        )
+
+    comp_cancel.autocomplete("contest")(autocomplete_contest(CompState.SUBMIT))
+
     @comp.subcommand("show")
-    async def comp_show(self, ctx: InteractionContext):
+    @slash_option(
+        "contest",
+        "Choose a contest to display.",
+        OptionTypes.INTEGER,
+        required=True,
+        autocomplete=True,
+    )
+    async def comp_show(self, ctx: InteractionContext, contest: int):
         """Display a competition accomponied by a submit button."""
+        comps = database.get_guild_comps(ctx.guild_id, CompState.SUBMIT)
+        if contest not in [comp[0] for comp in comps]:
+            await ctx.send(embed=error_embed("That contest is not availible."))
+            return
+        comp = [comp for comp in comps if comp[0] == contest][0]
+        await ctx.send(embed=comp_embed(comp), components=submit_button(contest))
+
+    comp_show.autocomplete("contest")(autocomplete_contest(CompState.SUBMIT))
+
 
 def setup(bot: Client):
     Manage(bot)
